b874ad3b0e28ac01d80afd97a2e5c374f963ef0b
Simphiwe06 <simphiwembuli06@gmail.com>
2023-08-04 12:32:37 +0200

    separated Javascript files and fixed a few problems

--- scripts.js
+++ scripts.js
@@ -1,16 +1,5 @@
-// configuration.js
-
-const company = 'ACME Inc.'
-const year = 2022
-export { company, year };
-
-// scripts.js
-
 import { company, year } from './configuration';
 
-const message = `© ${company} (${year})`;
+const message = '© ' + company + ' (' + year + ')';
+document.querySelector('footer').innerText = message;
 
-// used EventListner //
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('footer').innerText = message;
-  });
